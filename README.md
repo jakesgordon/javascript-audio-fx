2c43afcc7f76795fada3f35bd3f074879d221add
Jake Gordon <jake@codeincomplete.com>
2011-09-17 18:21:29 -0700

    initial version

---
+++ README.md
@@ -0,0 +1,28 @@
+Javascript AudioFX (v0.0.1)
+===========================
+
+Experimenting with a tiny library to abstract away the complications with HTML5 Audio.
+
+Similar to [buzz](http://buzz.jaysalvat.com/) but simpler and include support
+for pools of audio tags (for short FX that need to be played multiple overlapping times)
+
+Very Early experimentation, no documentation or blog articles yet.
+
+YMMV
+
+License
+=======
+
+See [LICENSE](https://github.com/jakesgordon/javascript-audio-fx/blob/master/LICENSE) file.
+
+Contact
+=======
+
+If you have any ideas, feedback, requests or bug reports, you can reach me at
+[jake@codeincomplete.com](mailto:jake@codeincomplete.com), or via
+my website: [Code inComplete](http://codeincomplete.com/)
+
+
+
+
+
---
+++ audio-fx.js
@@ -0,0 +1,115 @@
+AudioFX = function() {
+
+  //---------------------------------------------------------------------------
+
+  var VERSION = '0.0.1';
+
+  //---------------------------------------------------------------------------
+
+  var hasAudio = false, audio = document.createElement('audio'), audioSupported = function(type) { var s = audio.canPlayType(type); return (s === 'probably') || (s === 'maybe'); };
+  if (audio && audio.canPlayType) {
+    hasAudio = {
+      ogg: audioSupported('audio/ogg; codecs="vorbis"'),
+      mp3: audioSupported('audio/mpeg;'),
+      wav: audioSupported('audio/wav; codecs="1"'),
+      loop: (typeof audio.loop === 'boolean') // some browsers (FF) dont support loop yet
+    }
+  }
+
+  //---------------------------------------------------------------------------
+
+  var create = function(src, options, onload) {
+
+    var audio = document.createElement('audio');
+
+    if (onload) {
+      var ready = function() {
+        audio.removeEventListener('canplay', ready, false);
+        onload();
+      }
+      audio.addEventListener('canplay', ready, false);
+    }
+
+    if (options.loop && !hasAudio.loop)
+      audio.addEventListener('ended', function() { audio.currentTime = 0; audio.play(); }, false);
+
+    audio.volume = options.volume || 0.2;
+    audio.loop   = options.loop;
+    audio.src    = src;
+
+    return audio;
+  }
+
+  //---------------------------------------------------------------------------
+
+  var wrapper = function(src, options, onload) {
+
+    var pool = [];
+    if (hasAudio) {
+      for(var n = 0 ; n < (options.pool || 1) ; n++)
+        pool.push(create(src, options, n == 0 ? onload : null));
+    }
+    else {
+      onload();
+    }
+
+    var find = function() {
+      var n, audio;
+      for(n = 0 ; n < pool.length ; n++) {
+        audio = pool[n];
+        if (audio.paused || audio.ended)
+          return audio;
+      }
+    };
+
+    return {
+
+      audio: (pool.length == 1 ? pool[0] : pool),
+
+      play: function() {
+        var audio = find();
+        if (audio)
+          audio.play();
+      },
+
+      stop: function() {
+        var n, audio;
+        for(n = 0 ; n < pool.length ; n++) {
+          audio = pool[n];
+          audio.pause();
+          audio.currentTime = 0;
+        }
+      }
+    };
+
+  };
+
+  //---------------------------------------------------------------------------
+
+  var factory = function(name, options, onload) {
+    options = options || {};
+
+    var src = name, formats = options.formats;
+    if (formats) {
+      for(var n = 0 ; n < formats.length ; n++) {
+        if (hasAudio && hasAudio[formats[n]]) {
+          src = src + '.' + formats[n];
+          break;  
+        }
+      }
+    }
+
+    return wrapper(src, options, onload);
+  };
+
+  //---------------------------------------------------------------------------
+
+  factory.version = VERSION;
+  factory.enabled = hasAudio;
+
+  return factory;
+
+  //---------------------------------------------------------------------------
+
+}();
+
